7d6af8b4e5d60d97cc66fee32e93e115f059b656
GuuNguyem <guuhighschool@gmail.com>
2023-06-10 08:09:20 +0700

    restaurant detail

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:review_restaurant/screens/city_selection_screen.dart';
-import 'package:review_restaurant/screens/home_screen.dart';
-import 'package:review_restaurant/screens/restaurant_list_screen.dart';
-import 'package:review_restaurant/screens/restaurant_detail_screen.dart';
+// import 'package:review_restaurant/screens/city_selection_screen.dart';
+// import 'package:review_restaurant/screens/home_screen.dart';
+// import 'package:review_restaurant/screens/restaurant_list_screen.dart';
+// import 'package:review_restaurant/screens/restaurant_detail_screen.dart';
 import 'package:review_restaurant/screens/welcome_screen.dart';
 
 void main() {
